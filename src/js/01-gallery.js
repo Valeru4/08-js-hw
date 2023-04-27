031ba5c2b9a33e83c63d843a6a24e5deadfc80e9
Valeriya Pavlova <vla.valeriya@gmail.com>
2023-04-27 19:26:12 +0300

    update 1 and 2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items'
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+
+const galleryEl = document.querySelector('.gallery')
+
+const imgItemEl = createImageItemEl(galleryItems)
+
+
+galleryEl.insertAdjacentHTML('beforeend', imgItemEl)
+
+function createImageItemEl(images) {
+    return images.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+                                    <a class="gallery__link" href="${original}">
+                                        <img
+                                        class="gallery__image"
+                                        src="${preview}"
+                                        alt="${description}"
+                                        />
+                                    </a>
+                                </li>`;
+    }).join('');
+}
+
+
+
+galleryEl.addEventListener('click', onImageClick)
+
+function onImageClick() {
+    // event.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery__item a', 
+    {captionDelay: 250,  captionsData: 'alt', enableKeyboard: true});   
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+
+const videoPlayer = document.getElementById('vimeo-player');
+const player = new Player(videoPlayer);
+
+player.on('timeupdate', throttle(onPlay, 1000))
+
+function onPlay(data) {
+    localStorage.setItem("videoplayer-current-time" , data.seconds)
+}
+
+
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime) {
+  player.setCurrentTime(currentTime)
+    .then((seconds) => {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch((error) => {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
